b32aaedbef1cd4c04191ae64d65a9f4de7876f60
Yun119server <gildong28@naver.com>
2026-03-12 18:09:11 +0900

    dp 2579

--- category/dp/2579.go
+++ category/dp/2579.go
@@ -25,9 +25,10 @@ func main() {
 	}
 
 	dp[0] = stairs[0]
-	dp[1] = stairs[1]
-	for i := 2; i < n; i++ {
-		dp[i] = max(dp[i-1], dp[i-2]) + stairs[i]
+	dp[1] = stairs[0] + stairs[1]
+	dp[2] = max(stairs[0]+stairs[2], stairs[1]+stairs[2])
+	for i := 3; i < n; i++ {
+		dp[i] = max(dp[i-3]+stairs[i-1]+stairs[i], dp[i-2]+stairs[i])
 	}
 	fmt.Printf("%d\n", dp[n-1])
 }
